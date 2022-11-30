5220aadedcea7dc89161770b1ef04698fe560b45
Duskycupid <118800276+Duskycupid@users.noreply.github.com>
2022-11-30 11:26:16 +0530

    encapsulation

---
+++ Area.java
@@ -0,0 +1,21 @@
+//221130 Shyam
+package example;
+
+public class Area 
+{
+	//fields to calculate area
+int length;
+	int breadth;
+	// constructor to initialize values
+	Area (int length,int breadth)
+	{
+		this.length = length;
+		this.breadth = breadth;
+	}
+// method to calculate area
+	public void getArea()
+	{
+		int area = length*breadth;
+		System.out.println ("Area:"+ area);
+	}
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+package example;
+
+public class Main {
+	public static void main (String [] args)
+	{
+		// create object of area
+		// pass value of length and breadth
+		Area rectangle = new Area (5,6);
+		rectangle.getArea();
+	}
+	
+
+}
